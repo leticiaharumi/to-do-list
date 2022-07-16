05f875a143072ba058340e0703908c9a09182e91
Leticia Harumi <80927546+leticiaharumi@users.noreply.github.com>
2022-07-15 22:29:26 -0300

    To do list

---
+++ scripts.js
@@ -0,0 +1,27 @@
+const input = document.querySelector('.input')
+const ulContainer = document.querySelector('.ulContainer')
+const lixeira = document.querySelector('.lixeira')
+
+input.addEventListener('submit', event =>{
+    event.preventDefault()
+
+    const inputValue = event.target.add.value.trim()
+    
+    if(inputValue.length){
+        ulContainer.innerHTML += `
+        <li class="li">
+            <span class="linha-li">${inputValue}</span>
+            <i class="bx bxs-trash lixeira"></i>
+        </li>
+        `   
+        event.target.reset()
+    }
+})
+
+ulContainer.addEventListener('click', event =>{
+    const clickElement = event.target
+
+    if(Array.from(clickElement.classList).includes('lixeira')){
+        clickElement.parentElement.remove()
+    }
+})
